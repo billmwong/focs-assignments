0b2857a355bc8568d213e930eca7a0fd0e806dba
billmwong <billmwong@gmail.com>
2016-09-13 19:44:36 -0400

    finished hw3

--- day4/hw3.rkt
+++ day4/hw3.rkt
@@ -1,11 +1,11 @@
 #lang racket
 
-;;; Student Name: Frankly Olin [change to your name]
+;;; Student Name: Bill Wong
 ;;;
 ;;; Check one:
 ;;; [ ] I completed this assignment without assistance or external resources.
-;;; [ ] I completed this assignment with assistance from ___
-;;;     and/or using these external resources: ___
+;;; [x] I completed this assignment with assistance from David Abrahams, Annabel Consilvio
+;;;     and/or using these external resources: Google
 
 ;;;;;;;;;;;
 ;; 1. assq
@@ -18,7 +18,34 @@
 ;; If the key is not found in the list, `assq` returns `#f`.
 
 
+(define (assq key lst)
+  (if (empty? lst)
+    #f
+    (if (eq? key (first (first lst)))
+      (first lst)
+      (assq key (rest lst))
+    )
+  )
+)
 
+(define operator-list
+  (list (list 'ADD +)
+        (list 'SUB -)
+        (list 'MUL *)
+        (list 'DIV /)
+        (list 'GT >)
+        (list 'LT <)
+        (list 'GE >=)
+        (list 'LE <=)
+        (list 'EQ =)
+        (list 'NEQ (lambda (x y) (not (= x y))))
+        (list 'ANND (lambda (x y) (and x y)))
+        (list 'ORR (lambda (x y) (or x y)))
+        (list 'NOTT not)))
+
+(assq 'ADD operator-list) ;--> '(ADD #<procedure:+>)
+(assq 'ANND operator-list) ;--> '(ANND #<procedure>)
+(assq 'FOO operator-list) ;--> #f
 
 
 ;;;;;;;;;;;
@@ -28,3 +55,36 @@
 ;;
 ;; Add the `lookup-list` argument to your hw2 evaluator (or ours, from the solution set).
 ;; `(evaluate 'foo lookup-list)` should return whatever `'foo` is associated with in `lookup-list`.
+
+; (define (calculate x)
+;   (if (number? x)
+;     x
+
+;   )
+; )
+
+
+(define (evaluate key lst)
+  (if (empty? lst)
+    #f
+    (if (eq? key (first (first lst)))
+      (second (first lst))
+      (evaluate key (rest lst))
+    )
+  )
+)
+
+(define (calculate x)
+  (if (number? x)
+    x
+    ((evaluate (first x) operator-list) (second x) (third x))
+  )
+)
+
+(display "Evaluate:\n")
+(evaluate 'ADD operator-list) ;--> '(ADD #<procedure:+>)
+(evaluate 'GT operator-list) ;--> '(ANND #<procedure>)
+(evaluate 'FOO operator-list) ;--> #f
+
+(calculate '(ADD 5 4)) ; -> 7
+(calculate '(MUL 5 4)) ; -> 20
