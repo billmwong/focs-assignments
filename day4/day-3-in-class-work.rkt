a4017f3912cc07781c6687d2502cfceb3b8ac697
billmwong <billmwong@gmail.com>
2016-09-13 17:33:07 -0400

    added stuff done in class

--- day4/day-3-in-class-work.rkt
+++ day4/day-3-in-class-work.rkt
@@ -2,11 +2,34 @@
 
 ;;;;;;;;;;
 ;;; Day 3 in class work
+;; Worked with David Abrahams, Annabel Consilvio
 
 ;;;;;;;;;;
 ;; 0.  Implement factorial both recursively and tail recursively.
 ;;     Hint:  The tail recursive version will use a helper function.
 
+(define (factorial num)
+
+  (cond
+    [(= num 0) 1]
+    [else (* num (factorial (- num 1)))]
+  )
+
+)
+
+(define (fact-tail num)
+  (fact-acum num 1)
+)
+
+(define (fact-acum num acum)
+  (cond
+    [(= num 0) acum]
+    [else (fact-acum (- num 1) (* acum num))]
+  )
+)
+
+(display (fact-tail 3)) (newline) ;; -> 10
+(display (filter even? '(1 2 3 4 5 6))) (newline) ;; -> 10
 
 ;;;;;;;;;;;
 ;; 1.  Filter is built in to scheme.
@@ -19,6 +42,20 @@
 ;; Implement it anyway.  You might want to call it my-filter?  What arguments does it take?
 
 
+(define (my-filter op lst)
+  (if (empty? lst) 
+    ; if empty
+    lst
+    
+    ; if not empty
+    (let ([filt-rest (my-filter op (rest lst))])
+      (if (op (first lst))
+        (cons (first lst) filt-rest)
+        filt-rest
+      )
+    )
+  )
+)
 
 
 
@@ -36,10 +73,21 @@
 ;; Implement it as well.  You might want to call it my-map.  What arguments does it take?
 
 
+(define (double x) (* 2 x))
 
+(define (my-map op lst)
+  (if (empty? lst) 
+    ; if empty
+    lst
+    
+    ; if not empty
+    (let ([map-rest (my-map op (rest lst))])
+        (cons (op (first lst)) map-rest)
+    )
+  )
+)
 
-
-
+(display (my-map double '(1 2 3 4 5 6))) (newline) ;; -> 10
 
 ;;;;;;;;;;;
 ;; 3.  While we're reimplementing built-ins, implement my-append (just like built in append)
